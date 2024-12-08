03f358dc2180773e4640347c43e2711b07844b93
luasenvy <luas.envy@gmail.com>
2024-12-08 20:22:53 +0900

    fix: python use path

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class NodeExtension(Extension):
       self.nodePath = subprocess.check_output(['which', 'node'], universal_newlines=True, stderr=subprocess.STDOUT).strip()
     except subprocess.CalledProcessError:
       try:
-        self.nodePath = subprocess.check_output(['nvm', 'which', 'current'], universal_newlines=True, stderr=subprocess.STDOUT).strip()
+        self.nodePath = subprocess.check_output(['nvm', 'which', 'current'], universal_newlines=True, shell=True, stderr=subprocess.STDOUT).strip()
       except subprocess.CalledProcessError:
         try:
           tryNodeVersion = subprocess.check_output(['/snap/bin/node', '-v'], universal_newlines=True, stderr=subprocess.STDOUT).strip()
